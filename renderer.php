7cda1c9f171c6fca9cb6c9b449c05f25a20d77bd
Daniel P. Seemuth <dps-github-moodle@seemuth.com>
2014-05-08 12:42:59 -0500

    Extend qtype_calculated_renderer without any overrides

--- renderer.php
+++ renderer.php
@@ -18,37 +18,24 @@
  * Calculated question renderer class.
  *
  * @package    qtype
- * @subpackage calculated
+ * @subpackage calculatedformat
  * @copyright  2011 The Open University
+ * @copyright  2014 Daniel P. Seemuth
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot . '/question/type/numerical/renderer.php');
+require_once($CFG->dirroot . '/question/type/calculated/renderer.php');
 
 
 /**
- * Generates the output for calculated questions.
+ * Generates the output for calculated questions with number formatting.
  *
  * @copyright  1999 onwards Martin Dougiamas {@link http://moodle.com}
+ * @copyright  2014 Daniel P. Seemuth
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class qtype_calculated_renderer extends qtype_numerical_renderer {
-    public function correct_response(question_attempt $qa) {
-        $question = $qa->get_question();
-        $answer = $question->get_correct_response();
-        if (!$answer) {
-            return '';
-        }
-
-        $response = $answer['answer'];
-        if ($question->unitdisplay != qtype_numerical::UNITNONE && $question->unitdisplay != qtype_numerical::UNITINPUT) {
-            $response = $question->ap->add_unit($response);
-        }
-
-        return get_string('correctansweris', 'qtype_shortanswer', $response);
-    }
-
+class qtype_calculatedformat_renderer extends qtype_calculated_renderer {
 }
